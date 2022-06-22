407776053e9526e86dc908c1fd2584ef4fcf1c8d
rosemary <abiga@DESKTOP-VU4J4NC.net>
2022-06-22 17:26:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var opr = prompt("operator: ")
+var num1 = parseInt(prompt("number 1: "))
+var num2 = parseInt(prompt("number 2: "))
+
+if (opr == "+") {
+    alert(num1 + num2)
+} else if (opr == "_") {
+    alert(num1 - num2)
+} else if (opr == "*") {
+    alert(num1 * num2)
+} else if (opr == "/") {
+    alert(num1 / num2)
+}
